31dbe464e9b60aef92f5e1073edb7cb3c4504d9d
leitec <leitec>
2005-05-31 14:06:04 +0000

    Licensing info was slightly off.

--- lists.c
+++ lists.c
@@ -1,33 +1,3 @@
-/*
- * Copyright (C) 2005, Claudio Leite All rights reserved.
- * 
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions are
- * met:
- * 
- * * Redistributions of source code must retain the above copyright
- * notice, this list of conditions and the following disclaimer.
- * 
- * * Redistributions of source code must retain the above copyright
- * notice, this list of conditions and the following disclaimer.
- * 
- * * Neither the name of the BSF Software Project nor the names of its
- * contributors may be used to endorse or promote products derived from
- * this software without specific prior written permission.
- * 
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS
- * IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, 
- * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
- * PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR
- * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
- * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
- * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
- * PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
- * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
- * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
- * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE. 
- */
-
 #include "bsfirc.h"
 
 extern struct BSFirc *bsfirc;
@@ -157,6 +127,24 @@ change_user_mode(char *name, char *chan, int plus, int mode)
 
 					break;
 				}
+			}
+		}
+	}
+}
+
+/* PROTO */
+void
+change_user_nick(char *old, char *new)
+{
+	struct ChannelList *tr;
+	struct UserList *utr;
+
+	for(tr = chanlist; tr != NULL; tr = tr->next) {
+		for(utr = tr->users; utr != NULL; utr = utr->next) {
+			if(strcasecmp(old, utr->name) == 0) {
+				free(utr->name);
+				utr->name = strdup(new);
+				break;
 			}
 		}
 	}
